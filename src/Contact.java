0be6774a45129f526a2a498419cef5da55bd7562
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-14 13:25:28 -0500

    before testing write

--- src/Contact.java
+++ src/Contact.java
@@ -25,4 +25,5 @@ public class Contact {
     public String toString() {
         return fullName + " | " + phone + " | ";
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static String filename = "contacts.txt";
     public static String currentDir = System.getProperty("user.dir");
     public static String directory = currentDir + "/data";
     public static Path filepath = Paths.get(directory,filename);
-    public static void main(String[] args) {
+    public static ArrayList<Contact> contactList = new ArrayList<>();
+
+    public static void addContact(Contact newContact) {
+        contactList.add(newContact);
+    }
+    public static void main(String[] args) throws IOException {
+        // Onload read the file contacts.txt
+        List<String> fileContactList = Files.readAllLines(filepath);
+
         Contact jenny = new Contact("Jenny", "Austin", "333-333-3333");
-        System.out.println(jenny);
+        Contact john = new Contact("John", "Smith", "333-333-4444");
+        addContact(jenny);
+        addContact(john);
+
+        Files.write(filepath,contactList);
+
+        System.out.println(contactList);
+        System.out.println(fileContactList);
     } // main method
 } // main class
