0961a4f16cc1bed84191af92aff4722e6ab77b5f
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-14 12:40:58 -0500

    created getters/constructor for contact class and tested

--- src/Contact.java
+++ src/Contact.java
@@ -1,4 +1,28 @@
 public class Contact {
-    private String name;
+    private String firstName;
+    private String lastName;
+    private String fullName;
     private String phone;
+
+    public Contact(String firstName, String lastName, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.fullName = firstName + " " + lastName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String toString() {
+        return fullName + " | " + phone + " | ";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
+    public static String filename = "contacts.txt";
+    public static String currentDir = System.getProperty("user.dir");
+    public static String directory = currentDir + "/data";
+    public static Path filepath = Paths.get(directory,filename);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        Contact jenny = new Contact("Jenny", "Austin", "333-333-3333");
+        System.out.println(jenny);
+    } // main method
+} // main class
