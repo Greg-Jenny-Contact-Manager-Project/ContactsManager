7881e6da1f837760603130cd5c9071ad8950765e
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-14 19:07:26 -0500

    updated search to be case insensitive, changed search printout when more than one contact is found, diplay contacts list alphabetically by first name, added header to contacts list

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -1,6 +1,7 @@
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -12,6 +13,7 @@ public class ContactManager {
     public static String directory = currentDir + "/data";
     public static Path filepath = Paths.get(directory, filename);
     public static List<String> contactList = new ArrayList<>();
+
 
     public static void addToContactList(Contact newContact) {
         if (contactList.contains(String.valueOf(newContact))) {
@@ -45,16 +47,17 @@ public class ContactManager {
         printMainMenu(sc);
     }
 
-    public static void deleteContact() {
+    public static void deleteContact(Scanner sc) {
 
     }
 
+// this searches & prints an array list of all contacts that match the search string.
     public static void searchContact(Scanner sc) {
         System.out.println("Enter the name of the contact");
         String contact = sc.nextLine();
         List<String> result = contactList.stream()
-                .filter(x -> x.contains(contact)).collect(Collectors.toList());
-        System.out.println(result);
+                .filter(x -> x.toLowerCase().contains(contact.toLowerCase())).collect(Collectors.toList());
+        result.forEach(System.out::println);
         System.out.println("---------------");
         System.out.println("1. Search another contact\n" +
                 "2. Return to main menu\n");
@@ -66,11 +69,12 @@ public class ContactManager {
                 break;
             case 2:
                 printMainMenu(sc);
-    }
+        }
 
     }
 
     public static void printList(Scanner sc) {
+        Collections.sort(contactList);
         contactList.forEach(System.out::println);
         System.out.println("----------------------------------");
         System.out.println("1. Return to main menu\n" +
@@ -114,12 +118,12 @@ public class ContactManager {
             case 3:
                 searchContact(sc);
                 break;
-//            case 4:
-//                category = "Meat";
-//                break;
-//            case 5:
-//                category = "Other";
-//                break;
+            case 4:
+                deleteContact(sc);
+                break;
+            case 5:
+                System.out.println("Thank you for using the contact manager");
+                break;
         }
     }
 }// ContactManager CLASS
