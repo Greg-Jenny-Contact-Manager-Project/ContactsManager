c151676f2bd7852ce483a466a4d587e9d45b15e9
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-15 22:24:56 -0500

    create searchFunction to seperate search functionality for use in other methods

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -6,14 +6,14 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
+// TODO: 10/15/22 formatting for all the strings centered etc.
 public class ContactManager {
     public static String filename = "contacts.txt";
     public static String currentDir = System.getProperty("user.dir");
     public static String directory = currentDir + "/data";
     public static Path filepath = Paths.get(directory, filename);
     public static List<String> contactList = new ArrayList<>();
-    
-
+//    public static List<String> result;
     // Take in new Contact object and add it to the ArrayList of all contacts as a STRING
     public static void addToContactList(Contact newContact) {
         if (contactList.contains(String.valueOf(newContact))) {
@@ -23,7 +23,7 @@ public class ContactManager {
         }
     }
 
-
+    // TODO: 10/15/22 Verify contact does not already exist before adding new contact
     // Creates a new Contact object via user inputs in the terminal
     public static void addContact(Scanner sc) {
         String firstName;
@@ -43,40 +43,45 @@ public class ContactManager {
     }
 
     // TODO: 10/15/22 need to adjust for an option to choose which selection to delete.
-    // TODO: 10/15/22 If none found return statement "no contact found" and menu
 
     public static void deleteContact(Scanner sc) {
         System.out.println("\n" + Menus.breakPt + "\n                    DELETE CONTACT\n" + Menus.breakPt);
         try {
-            System.out.println("Enter the name of the contact you would like to delete");
-            String contact = sc.nextLine();
-            List<String> result = contactList.stream()
-                    .filter(x -> x.toLowerCase().contains(contact.toLowerCase())).collect(Collectors.toList());
-            result.forEach(System.out::println);
+            List<String> result = searchfunction(sc);
             int deleteIndex = contactList.indexOf(result.get(0));
             contactList.remove(deleteIndex);
-
+            Menus.returnMenu(sc, "1. Delete another contact", ContactManager::deleteContact);
         } catch (IndexOutOfBoundsException e) {
             System.out.println(Menus.breakPt + "\n\nThat name does not match any contact in the GREY");
+            Menus.returnMenu(sc, "1. Return to Delete Contact", ContactManager::deleteContact);
         }
          // this works only when there is a single entry returned, otherwise you delete the first on the list.
-        Menus.returnMenu(sc, "1. Delete another contact", ContactManager::deleteContact);
-
     }
 
 
     // Searches contactList ArrayList based off a user input String, prints an array list of any Contact that has values that match the input. ignores case.
     public static void searchContact(Scanner sc) {
         System.out.println("\n" + Menus.breakPt + "\n                    SEARCH CONTACT\n" + Menus.breakPt);
-        System.out.println("Enter the name of the contact");
-        String contact = sc.nextLine();
-        List<String> result = contactList.stream()
-                .filter(x -> x.toLowerCase().contains(contact.toLowerCase())).collect(Collectors.toList());
+        List<String> result = searchfunction(sc);
         System.out.println(Menus.breakPt + "\n");
-        result.forEach(System.out::println);
+        if (!result.isEmpty()) {
+            Menus.returnMenu(sc, "1. Search another contact", ContactManager::searchContact);
+        } else {
+            System.out.println("No contact found");
+        }
         Menus.returnMenu(sc, "1. Search another contact", ContactManager::searchContact);
-
     }
+public static List<String> searchfunction(Scanner sc) {
+    List<String> result;
+    System.out.println("Enter the name of the contact");
+    String contact = sc.nextLine();
+        result = contactList.stream()
+                .filter(x -> x.toLowerCase().contains(contact.toLowerCase())).collect(Collectors.toList());
+        result.forEach(System.out::println);
+        return result;
+    }
+
+
 
     // Prints the contactList in alphabetical order based on first name values
     public static void printList(Scanner sc) {
